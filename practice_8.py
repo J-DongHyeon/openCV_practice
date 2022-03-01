ffe84d2bf98a213055287d0707df6397902843cf
DESKTOP-MG12BQR\jdh38 <jdh3876@naver.com>
2022-03-01 20:31:43 +0900

    end; prac_8

--- practice_8.py
+++ practice_8.py
@@ -5,3 +5,18 @@ Created on Tue Mar  1 20:09:40 2022
 @author: jdh38
 """
 
+import numpy as np
+import cv2
+#import matplotlib.pyplot as plt
+
+img = cv2.imread('Lenna.png', 0)
+
+cv2.imshow('img', img)
+
+key = cv2.waitKey(0)
+
+if (key == 27) :
+    cv2.destroyAllWindows()
+elif (key == ord('s')) :
+    cv2.imwrite('Lenna_saved.png', img)
+    cv2.destroyAllWindows()
