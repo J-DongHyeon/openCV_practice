8a358d0c1c7ac9b1abb7eaec4ad823e6df80e773
DESKTOP-MG12BQR\jdh38 <jdh3876@naver.com>
2022-03-01 22:00:25 +0900

    end; prac_10

--- practice_10.py
+++ practice_10.py
@@ -5,3 +5,39 @@ Created on Tue Mar  1 20:51:54 2022
 @author: jdh38
 """
 
+import numpy as np
+import cv2
+
+freezeFlag = False
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+def onMouseEvent(event, x, y, flags, userdata) :
+    global freezeFlag
+    
+    if event == cv2.EVENT_LBUTTONDOWN :
+        freezeFlag = False
+    elif event == cv2.EVENT_RBUTTONDOWN :
+        freezeFlag = True
+
+cv2.namedWindow('videoWindow')
+cv2.setMouseCallback('videoWindow', onMouseEvent)
+cap = cv2.VideoCapture('aespa_opencv.mp4')
+
+while(cap.isOpened()) :
+    if not freezeFlag :
+        ret, frame = cap.read()
+        frameCopy = frame.copy()
+        cv2.putText(frame, 'Playing', (10, 50), font, 1, (0, 0, 255), 3)
+        cv2.imshow('videoWindow', frame)
+    
+    else : 
+        cv2.putText(frameCopy, 'Stopped', (10, 50), font, 1, (0,255,0), 3)
+        cv2.imshow('videoWindow', frameCopy)
+        
+    key = cv2.waitKey(10)
+    print('freezeFlag ', freezeFlag)
+    if key == 27:
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
